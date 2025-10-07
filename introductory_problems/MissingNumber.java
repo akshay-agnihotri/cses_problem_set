b2a86d3d7d05b442f4fc95856a032fa90b769d6a
akshay agnihotri <agnihotriakshay36@gmail.com>
2025-10-08 00:41:45 +0530

    Add MissingNumber solution to introductory problems

---
+++ introductory_problems/MissingNumber.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class MissingNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long n = sc.nextLong();
+        long sum = (n * (n + 1)) / 2;
+        for(int i=0; i<n-1; ++i){
+            sum -= sc.nextInt();
+        }
+        System.out.println(sum);
+    }
+}
